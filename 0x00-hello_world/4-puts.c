cd09be9be570d0dc2b5d6f895f7dd2910b924dd1
Coder1967 <nosakhareaghedo42@gmail.com>
2022-04-07 14:59:45 -0700

    words

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,19 @@
+
+#include <stdio.h>
+
+
 /**
  * main - Entry point
  *
  * Return Always 0(success)
 */
 
-#include <stdio.h>
+
 int main(void)
 {                
         
-puts ("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
 
+#include <stdio.h>
