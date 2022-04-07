a88602ef90e0ca089afb8aafdd83d5233b215c06
Coder1967 <nosakhareaghedo42@gmail.com>
2022-04-07 15:23:53 -0700

    awesome coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,5 +4,10 @@
  *
  * Return:Always 0(success)
  */
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
+
