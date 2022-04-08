10370cac39689bd7e41b6162175f1f5bed01dc4b
Coder1967 <nosakhareaghedo42@gmail.com>
2022-04-08 16:21:37 -0700

    numeberz

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return:Always 0(success)
+ */
+int main(void)
+{
+	char ch = 'z';
+
+	while ( ch >= 'a')
+	{
+		putchar(ch);
+
+		ch--;
+	}
+	putchar('\n');
+	return (0);
+}
+
