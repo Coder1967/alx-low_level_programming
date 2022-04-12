2e7a513c016e3684b8c7e942910d2e6184d2ce9b
Coder1967 <nosakhareaghedo42@gmail.com>
2022-04-12 04:20:44 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -Entry point
+ *
+ * Return: 0(success)
+ */
+int main(void)
+{
+	int rep = 1;
+	while (rep <= 10)
+	{
+		char ch = 'a';
+		while (ch <= 'z')
+		{
+			putchar(ch);
+			++ch;
+		}
+		putchar('\n');
+		++rep;
+	}
+	return (0);
+}
