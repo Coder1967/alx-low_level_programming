f04cc2df66ef02a903e1f49620d3eeaf1eefc59e
Coder1967 <nosakhareaghedo42@gmail.com>
2022-04-12 15:42:57 -0700

    lastdigit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "main.h"
+/**
+ * print_last_digit - Entry point
+ * @n: compare 
+ * Return: Always 0
+ */
+  int print_last_digit(int n)
+{
+	int lastDigit = n % 10;
+	return (lastDigit);
+}
