244d961497aa3e31a51f3d4ed614ca024edf4303
Coder1967 <nosakhareaghedo42@gmail.com>
2022-04-12 16:23:05 -0700

    bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	for (i = 00; i <= 23; ++i)
+	{
+		for (j = 01; j <= 59 ++j)
+		{
+			printf("%d : %d", i, j);
+		}
+		putchar('\n');
+	}
