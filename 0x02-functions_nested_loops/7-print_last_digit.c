4dee6ac842baea08134a2ebebc86aeaddec5c2c9
Coder1967 <nosakhareaghedo42@gmail.com>
2022-04-12 16:01:45 -0700

    lastdigit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,13 +3,14 @@
 #include "main.h"
 /**
  * print_last_digit - Entry point
- * @n: compare 
+ * @n:compare 
  * Return: Always 0
  */
-  int print_last_digit(int n)
+int print_last_digit(int n)
 {
 	int lastDigit;
-	if(n < 0)
+
+	if (n < 0)
 	{
 		lastDigit = (-1 * (n % 10));
 		_putchar (lastDigit + '0');
