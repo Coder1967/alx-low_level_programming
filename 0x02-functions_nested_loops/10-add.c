4a92c9a240d35425c785b872cd5c71e6b0f47021
Coder1967 <nosakhareaghedo42@gmail.com>
2022-04-12 16:59:09 -0700

    addition

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-
+/**
+ * add - Entry point
+ * Return:Always 0
+ */
 int add(int a, int b)
 {
 	int num = a + b;
-	return num;
+	return (num);
 }
