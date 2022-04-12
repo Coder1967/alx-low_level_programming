4eea6d397dfdd90aa3877f3bd45150fb4f30d4d5
Coder1967 <nosakhareaghedo42@gmail.com>
2022-04-12 15:16:38 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _abs Entry point
+ * @n: value to be compared
+ * Return: Always 0
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-1)*n;
+	}
+	else if ( n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (0);
+	}
+}
