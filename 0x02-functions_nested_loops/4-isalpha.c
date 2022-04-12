911793e7defec65abbe92ba20f85753a00674867
Coder1967 <nosakhareaghedo42@gmail.com>
2022-04-12 14:31:06 -0700

    alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /**
  * _isalpha - Entry point
- * 
  * @c: contains value to be compared
  *
  * Return: Always 0(success)
  */
 int _isalpha(int c)
 {
-	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z' ))
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
 	{
 		return (1);
 	}
