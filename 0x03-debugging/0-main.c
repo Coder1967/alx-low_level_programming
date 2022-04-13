608c24430acc8223f9a19b329e34a5ddccc7b2dd
Coder1967 <nosakhareaghedo42@gmail.com>
2022-04-13 15:40:48 -0700

    added a readme

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,15 @@
 #include "main.h"
-
+#include <time.h>
 /**
  * main - tests function that checks if integer is positive or negative
  * Return: 0
  */
-
 int main(void)
 {
 	int i;
 
-	i = 98;
+	i = 0;
+
 	positive_or_negative(i);
 
 	return (0);
--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -17,6 +17,7 @@ void positive_or_negative(int i)
 	else if ( i == 0)
 	{
 		printf("%d is zero\n", i);
+	}
 	else
 	{
 		printf("%d is positive\n", i);
