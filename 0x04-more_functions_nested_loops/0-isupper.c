d25dd5bdd8d338c44267eb4828803c4b4407fd69
Coder1967 <nosakhareaghedo42@gmail.com>
2022-04-14 13:47:09 -0700

    uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * _isupper - Function to check for uppercases
+ * @c: variable to store value
+ * Return: 0
+ */
+int _isupper(int c)
+{
+	int letter;
+
+	if (isupper(c))
+	{
+		letter = 1;
+	}
+	else
+	{
+		letter = 0;
+	}
+	return (letter);
+}
+		
