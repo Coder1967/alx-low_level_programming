1fd088bfcacc05093bfcad2c515168e1339f5823
Coder1967 <nosakhareaghedo42@gmail.com>
2022-04-13 17:01:24 -0700

    avoided a while loop.

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+
 /**
  * main - causes an infinite loop
  * Return: 0
@@ -10,7 +11,6 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-
 	i = 0;
 
 	/*while (i < 10)
