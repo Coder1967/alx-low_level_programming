1528694ff99ac1347f3ef01cc71d9dddbb2c4b3d
Coder1967 <nosakhareaghedo42@gmail.com>
2022-04-14 10:12:34 -0700

    leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -24,9 +24,9 @@ void print_remaining_days(int month, int day, int year)
 			}
 		}
 	}
-	else if (month == 2 && day == 60)
+	else if (month == 2 && day == 60 && year % 4 != 0)
 	{
-		printf("Invaid date: %02d/%02d/%04d\n", month, day - 31, year);
+		printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 	}
 	else
 	{
