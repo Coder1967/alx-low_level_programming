ef764356d8c52d6efab6def81fd4fd091b482ed0
Coder1967 <nosakhareaghedo42@gmail.com>
2022-04-14 15:05:36 -0700

    multi

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * mul - function to multiply
+ * @a: variable to store first number
+ * @b: variable to store second number
+ */
+int mul(int a, int b)
+{
+	int multi = a * b;
+
+	return (multi);
+}
