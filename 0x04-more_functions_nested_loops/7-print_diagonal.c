1bfd73d3bc109777deb1b5e32d5aa8c5bd3a7729
Coder1967 <nosakhareaghedo42@gmail.com>
2022-04-14 18:36:47 -0700

    slashes

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_diagonal - prints diagonal lines
+ * @n: determines diagonal's amount
+ * Return: 0
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int b;
+
+	if (n > 0)
+	{
+		for ( i = 0; i < n; i++)
+		{
+			for (b = 0; b < i; b++)
+			{
+				-putchar (' ');
+			}
+			_putchar (92);
+			_putchar ('\n');
+		}
+	}
+	else
+	{
+		_putchar ('\n');
+	}
+
+}
