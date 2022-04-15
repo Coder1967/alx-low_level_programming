88a12fb3a9c746f2c87efd4fefe7f790369f3da2
Coder1967 <nosakhareaghedo42@gmail.com>
2022-04-14 18:13:46 -0700

    d

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,17 +10,16 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++)
+	if !(n <= 0 )
 	{
-		if ( n <= 0)
+		for (i = 0; i <= n; i++)
 		{
-			('\n');
-			break;
+			_putchar(i);
 		}
-		else
-		{
-			_putchar('_');
-		}
+		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+	{
+		_putchar('\n');
+	}
 }
