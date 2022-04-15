c528943f224668ab5bc338dd2505728f5335ef7e
Coder1967 <nosakhareaghedo42@gmail.com>
2022-04-14 17:58:40 -0700

    horiontal line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,14 +12,14 @@ void print_line(int n)
 
 	for (i = 0; i <= n; i++)
 	{
-		if ( n <=0)
+		if ( n <= 0)
 		{
 			_putchar('\n');
-			break;
 		}
 		else
 		{
-		_putchar('_');
+			_putchar('_');
+		}
 	}
 	_putchar('\n');
 }
