4cedd7994ab8faa9be6ea7a8e4f2182aa053e0de
Coder1967 <nosakhareaghedo42@gmail.com>
2022-04-15 04:26:08 -0700

    fb

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,15 +16,15 @@ int main(void)
 		}
 		else if (a % 3 == 0)
 		{
-			printf("Fizz ");
+			printf(" Fizz ");
 		}
 		else if (a % 5 == 0)
 		{
-			puts("Buzz ");
+			puts(" Buzz ");
 		}
 		else
 		{
-			printf("%2d", a);
+			printf(" %d ", a);
 		}
 	}
 	printf("\n");
