bcf449466b731c6c950009f86d5790283870704d
Coder1967 <nosakhareaghedo42@gmail.com>
2022-04-20 07:58:39 -0700

    swapping int values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - swaps integers
+ * @a: contains first integer to be swapped
+ * @b: contains second int to be swapped
+ * Return: 0
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
