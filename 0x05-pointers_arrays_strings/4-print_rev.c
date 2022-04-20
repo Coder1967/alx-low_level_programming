edd9d0811878f83a9ade7edba0b8b870dd220e14
Coder1967 <nosakhareaghedo42@gmail.com>
2022-04-20 12:52:58 -0700

    reversal

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,12 +10,12 @@ void print_rev(char *s)
 {
 	int a = 0;
 
-	while(s[a] != '\0')
+	while (s[a] != '\0')
 	{
 		a++;
 	}
 
-	while((a - 1) >= 0)
+	while ((a - 1) >= 0)
 	{
 		printf("%c", s[(a - 1)]);
 		a--;
