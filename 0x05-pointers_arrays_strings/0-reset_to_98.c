da5e163ab21368ac7384ae5af6795767638e06c2
Coder1967 <nosakhareaghedo42@gmail.com>
2022-04-20 07:18:14 -0700

    changing value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * reset_to_98 - function to cahange value
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+	 n = 98;
+	_putchar(n);
+}
