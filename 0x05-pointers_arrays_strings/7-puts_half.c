29626e66a36606809d765a9253571fb7e36a299b
Coder1967 <nosakhareaghedo42@gmail.com>
2022-04-20 16:01:25 -0700

    halving

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 #include "main.h"
-
+/**
+ * puts_half - function that cuts strings into halves
+ * @str: variable that stres the string
+ * Return: void of the
+ */
 void puts_half(char *str)
 {
-	int j =(strlen(str) / 2);
+	int j = (strlen(str) / 2);
 	int i;
 	int l = strlen(str);
 
