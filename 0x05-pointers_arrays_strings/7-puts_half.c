4bda8ac28ed192505fc7d82888027e49e359c4be
Coder1967 <nosakhareaghedo42@gmail.com>
2022-04-20 15:41:09 -0700

    half print

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+void puts_half(char *str)
+{
+	int j =(strlen(str) / 2);
+	int i;
+	int l = strlen(str);
+
+	for (i = j; i < l; i++)
+	{
+		printf("%c", str[i]);
+	}
+}
+
