85edb6bfc2ad0f33467d3479e2876cad194573f3
Coder1967 <nosakhareaghedo42@gmail.com>
2022-04-20 08:15:32 -0700

    counting length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,13 @@
 #include "main.h"
 #include <string.h>
 #include <stdlib.h>
+/**
+ * _strlen - function to count characters in a string
+ * @s: string to be cunted
+ * REturn:0
+ */
 
 int _strlen(char *s)
 {
-	strlen(*s);
+	strlen(s);
 }
