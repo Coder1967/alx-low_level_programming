2150337c831718e1185c6166a366bef0fc8fd88a
Coder1967 <nosakhareaghedo42@gmail.com>
2022-04-20 17:34:45 -0700

    g

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,12 +4,14 @@
  * _strcpy - function to copy a string
  * @dest: variable to receive string
  * @src: variable to be copied
+ * Return: null
  */
 char *_strcpy(char *dest, char *src)
 {
 	char *ch = src;
+
 	dest = ch;
 
 printf("%s", dest);
-	return src;
+	return (src);
 }
