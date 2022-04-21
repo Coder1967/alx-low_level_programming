779350182fb819b2029c807f925f0e9f489328d8
Coder1967 <nosakhareaghedo42@gmail.com>
2022-04-20 17:07:01 -0700

    string copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcpy - function to copy a string
+ * @dest: variable to receive string
+ * @src: variable to be copied
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *ch = src;
+	dest = ch;
+
+	printf("%s", dest);
+	return src;
+}
