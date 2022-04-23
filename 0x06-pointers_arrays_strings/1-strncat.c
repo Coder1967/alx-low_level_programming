9041a7acbf1280314ac9c8e6465ea27c44113fac
Coder1967 <nosakhareaghedo42@gmail.com>
2022-04-23 04:37:41 -0700

    concatenating strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * strncat - function to join string
+ * @dest: first variable
+ * @src: sencond variable
+ * @n: int variable
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	return(strncat(dest, src, n);
+}
