0d64d70ff89ba56e0981ab52a164bd3035bdc4a7
Coder1967 <nosakhareaghedo42@gmail.com>
2022-04-24 14:29:58 -0700

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 /**
  * main - function to be read
- * Return: 0
  */
 int main(void)
 {
 	int n;
 	int a[5];
 	int *p;
-
+ /**
+  * write your line of code here...
+  * Remember:
+  * - you are not allowed to use a
+  *
+  * - you are not allowed to modify p
+  *   - only one statement
+  *   - you are not allowed to code anything else than this line of code
+  */
 	a[2] = 1024;
 	p = &n;
 	p[5] = 98;
 	printf("a[2] = %d\n", a[2]);
-	return (0);
+		return (0);
 }
