69dbd0d4f6e9165b6d13a6c46e9378c2e10eb810
Coder1967 <nosakhareaghedo42@gmail.com>
2022-04-26 13:19:17 -0700

    strbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strpbrk - function
+ * @s: var1
+ * @accept: var2
+ * Return: value
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	return (strpbrk(s, accept));
+}
