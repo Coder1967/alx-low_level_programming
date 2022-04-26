8df1c4fcdfd5d35cd1ae12a41d0b9277e3385a2c
Coder1967 <nosakhareaghedo42@gmail.com>
2022-04-26 12:30:05 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <string.h>
-
+/**
+ * _memset - function to carry task
+ * @s: var to store string
+ * @b: a char var
+ * @n: a int var
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 	char *s1 = memset(s,b,n);
