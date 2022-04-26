2391315d2302b34653571673906245434bc39821
Coder1967 <nosakhareaghedo42@gmail.com>
2022-04-26 12:43:40 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _memcpy - function
+ * @dest: var1
+ * @src: var2
+ * @n: var3
+ * Return: s
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *s = memcpy(dest, src, n);
+	
+	return (s);
+}
