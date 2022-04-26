844900047e3473f280f39632c558dfc8b6f62598
Coder1967 <nosakhareaghedo42@gmail.com>
2022-04-26 13:29:18 -0700

    searching for a string within a string

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strstr - function
+ * @haystack: var1
+ * @needle: var2
+ * Return: value
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	return (strstr(haystack, needle));
+}
