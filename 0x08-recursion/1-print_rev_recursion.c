609cc012588653fc045f605989ed0b352519646a
Coder1967 <nosakhareaghedo42@gmail.com>
2022-04-28 13:17:28 -0700

    reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * _print_rev_recursion
+ * @s: variable 1
+ * Return: nothing
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+		s++;
+		_print_rev_recursion(s);
+		s--;
+		putchar(*s);
+	}
+}
