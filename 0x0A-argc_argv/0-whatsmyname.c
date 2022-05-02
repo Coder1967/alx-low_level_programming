cca9b1f098ee73fcc22bdc39f9eb9c2aae0c7c9d
Coder1967 <nosakhareaghedo42@gmail.com>
2022-05-02 03:08:43 -0700

    myname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * @agrc:argument count
+ * @agrv:argument vector
+ * main - function
+ * Return:0
+ */
+int main(int agrc, *char agrv[])
+{
+	printf("%s", agrv[0]);
+	return (0);
+}
