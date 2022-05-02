eca217fa72a876fb8db950551341a97eee942538
Coder1967 <nosakhareaghedo42@gmail.com>
2022-05-02 05:08:48 -0700

    whats my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
+ * main - function
  * @argc:argument count
  * @argv:argument vector
- * main - function
  * Return:0
  */
 int main(int argc, char *argv[])
 {
 	(void) argc;
+
 	printf("%s\n", argv[0]);
 	return (0);
 }
