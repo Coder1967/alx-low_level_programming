9665a1f3682307568a8ab2056bac62ce144ac837
Coder1967 <nosakhareaghedo42@gmail.com>
2022-05-02 04:41:54 -0700

    fixed

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,10 +3,12 @@
  * main - calling function
  * @argc: argument counter
  * @argv: argument vector
+ * Return:0
  */
 int main(int argc, char *argv[])
 {
 	int i = 0;
+
 	while (i < argc)
 	{
 		puts(argv[i]);
