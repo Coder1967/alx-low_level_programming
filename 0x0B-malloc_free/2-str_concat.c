6cb1f3f008aa0260e251fba031587605d9a54652
Coder1967 <nosakhareaghedo42@gmail.com>
2022-05-05 13:05:58 -0700

    concat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+
+char *str_concat(char *s1, char *s2);
+{
+	char *ptr; int i = 0, k = 0, m, n, q =0;
+
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+	while (s2[k] != '\0')
+	{
+		k++;
+	}
+	l = k + i + 1;
+
+	ptr = (char *) calloc(l, sizeof(char));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (m = 0; m < i; m++)
+	{
+		ptr[m] = s1[m];
+	}
+	for (n = m; n <= (i + k); n++)
+	{
+		ptr[n] = s2[q];
+		q++;
+	}
+	return (ptr);
+}
