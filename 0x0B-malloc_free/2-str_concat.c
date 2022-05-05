6c84b2610add0557884b22fb8af865eb604dbb9f
Coder1967 <nosakhareaghedo42@gmail.com>
2022-05-05 13:56:45 -0700

    alloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,7 +9,6 @@ char *str_concat(char *s1, char *s2)
 {
 	char *ptr; int i = 0, k = 0, m, n, l, q = 0;
 
-
 	while (s1[i] != '\0')
 	{
 		i++;
@@ -19,8 +18,6 @@ char *str_concat(char *s1, char *s2)
 		k++;
 	}
 	l = k + i + 1;
-	if (s1 != NULL && s2 != NULL)
-	{
 	ptr = (char *) calloc(l, sizeof(char));
 
 	if (ptr == NULL)
@@ -36,36 +33,5 @@ char *str_concat(char *s1, char *s2)
 		ptr[n] = s2[q];
 		q++;
 	}
-	}
-	if (s1 == NULL || s2 == NULL)
-	{
-		if (s2 == NULL)
-		{
-			ptr = (char *) malloc(i * sizeof(char));
-			if (ptr == NULL)
-			{
-				return NULL;
-			}
-			for (m = 0; m <= i; m++)
-			{
-				ptr[m] = s1[m];
-			}
-		}
-		else if (s1 == NULL)
-		{
-			ptr = (char *) calloc(k, sizeof(char));
-			if (ptr == NULL)
-			{
-				return NULL;
-			}
-			for (m = 0; m <= k; m++)
-			{
-				ptr[m] = s2[m];
-			}
-		}
-	}
-
-
-	
 	return (ptr);
 }
