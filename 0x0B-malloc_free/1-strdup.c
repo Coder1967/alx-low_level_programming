c11cc479ea35896175d70ad3d2f05bbb337ed8c5
Coder1967 <nosakhareaghedo42@gmail.com>
2022-05-05 07:14:29 -0700

    calloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,32 +1,50 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
+
+#include <stdio.h>
+
+
 
 char *_strdup(char *str)
+
 {
-	char *ptr; int i, n;
-	if (str == NULL)
-	{
-		return NULL;
-	}
 
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	ptr = (char *) calloc(i, sizeof(char));
-	if (ptr == NULL)
-	{
-		return NULL;
-	}
-	else
-	{
+	        char *ptr; int i, n;
 
-	for (n = 0; n < i; n++)
-	{
-		ptr[n] = str[n];
-	}
-	}
-	return ptr;
+		        if (str == NULL)
+
+				        {
+
+						                return NULL;
+
+								        }
+
+
+
+			        while (str[i]  != '\0')
+
+					        {
+
+							                i++;
+
+									        }
+
+				        ptr = (char *)  calloc(i, sizeof(char));
+
+
+
+					        for (n = 0; n < i; n++)
+
+							        {
+
+									                ptr[n] = str[n];
+
+											        }
+
+						        return ptr;
+
+
+
 }
 
 
