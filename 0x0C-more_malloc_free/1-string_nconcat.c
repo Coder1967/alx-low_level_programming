f1455ad070ce28c898b55a2bc7aa2365c470db0e
Coder1967 <nosakhareaghedo42@gmail.com>
2022-05-09 09:17:29 -0700

    concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,7 +24,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	i = strlen(s1);
 	y = strlen(s2);
 	total = (i + y) + 1;
-	p = (char *) malloc(sizeof(char) * total);
+	p = (char *) realloc(NULL, sizeof(char) * total);
 	if (p == NULL)
 	{
 		return (NULL);
