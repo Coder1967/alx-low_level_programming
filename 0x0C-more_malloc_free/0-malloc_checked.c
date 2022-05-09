660c90443e38c2c33b6bf68cf6868ff75d520a90
Coder1967 <nosakhareaghedo42@gmail.com>
2022-05-09 04:45:20 -0700

    malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * malloc_checked - function that returns address
+ * @b: variable
+ * Return: 0
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p = malloc(b);
+
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return;
+}
