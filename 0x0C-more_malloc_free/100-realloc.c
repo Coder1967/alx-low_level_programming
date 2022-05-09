ceb0fd32be451eb66be74b33b8a1ebc4a1e60178
Coder1967 <nosakhareaghedo42@gmail.com>
2022-05-09 15:02:17 -0700

    realoc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,13 +11,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *clone, *relloc;
 	unsigned int i;
-	
+
 	if (ptr != NULL)
 		clone = ptr;
 	else
-	{
 		return (malloc(new_size)); 
-	}
 	if (new_size == old_size)
 		return (ptr);
 	if (new_size == 0 && ptr != NULL)
@@ -31,7 +29,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	for (i = 0; i < (old_size || i < new_size); i++)
 	{
 		*(relloc + i) = clone[i];
-											}
+	}
 	free(ptr);
 	return (relloc);
 }
