b4f7a8c70d172e34d22b25606a3fbde09826717d
nosakhare <nosakhareaghedo42@gmail.com>
2022-05-11 15:27:24 +0000

    dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,18 @@
+#ifndef DOH_H
+#define DOG_H
 /**
  * struct dog - template for dogs
  * @name: name of dog
  * @age: age of dog
  * @owner of dog
  */
+#define struct dog
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif
