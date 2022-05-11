7bb08710de3e477665f145aa18642670aa578a04
nosakhare <nosakhareaghedo42@gmail.com>
2022-05-11 15:40:53 +0000

    struct ponter

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 /**
  * init_dog - void function
- * @dog: pointer to struct dog
  * @name: name of dog
  * @age: age of dog
  * @owner: name of dog's owner
@@ -15,7 +14,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	{
 		return;
 	}
-	d -> name = name;
-	d -> age = age;
-	d -> owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
