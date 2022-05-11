3195ba3583669127d4cca99507383dd724eeddae
nosakhare <nosakhareaghedo42@gmail.com>
2022-05-11 10:44:37 +0000

    macro definitions

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ * Return: 0 on success
+ */
+int main(void)
+{
+	puts( __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,5 @@
+#define ABS(x)
+#if ABS(x) > 0
+x = -1(x);
+#else x = x
+#endif
