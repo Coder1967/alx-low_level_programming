63cf360c607bdce4f89c3acfe3889efa54904929
nosakhare <nosakhareaghedo42@gmail.com>
2022-05-11 21:01:26 +0000

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -4,6 +4,7 @@
  * new_dog - function to make new dog
  * @name: name of new dog
  * @age: age of new dog
+ * @owner: owner of dog
  * Return: value
  */
 dog_t *new_dog(char *name, float age, char *owner)
@@ -15,10 +16,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		return (NULL);
 	}
+	else
+	{
 	ptr->name = name;
 	ptr->age = age;
 	ptr->owner = owner;
+	}
 	return (ptr);
 }
-
-	
