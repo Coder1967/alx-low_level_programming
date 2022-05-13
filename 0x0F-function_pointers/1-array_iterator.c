de0ac2bb1269cb48ff9fea6f9f016d428d2114d9
nosakhare <nosakhareaghedo42@gmail.com>
2022-05-13 21:05:48 +0000

    printing iteratively

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,13 +1,24 @@
 #include "function_pointers.h"
 #include <stdlib.h>
 #include <stddef.h>
+/**
+ * array_iterator - function to loop through numbers
+ * @array: pointer to first address of the array
+ * @size: number of numbers in array
+ * @action: function pointer
+ * Return: nothing
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 	
-	if (action != NULL)
+	if (action == NULL)
+		return;
+	else
+	{
 		for (i = 0; i < size; i++)
 		{
 			(action(array[i]));
 		}
-} 
+	}
+}
