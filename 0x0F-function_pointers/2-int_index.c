409061b47756a80dbc8111c662ce5ce146d6a58c
nosakhare <nosakhareaghedo42@gmail.com>
2022-05-13 22:20:55 +0000

    comparisons

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,22 +5,23 @@
  * @array: pointer to array to be compared
  * @size: number of elements in array
  * @cmp: pointer to function
+ * Return: int
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;  
+	int i;
 
-    	if (size <= 0)   
-      	{       
-	      	return (-1);   
-      	}    
-       	if (cmp != NULL && array != NULL)    
-	       	for (i = 0; i < size; i++)  
-	     	{        
-		       	if (cmp(array[i]) != 0)    
-		   	{        
-			   	return (i);     
-		    	}   
-	      	}  
-    	return (-1);  
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	if (cmp != NULL && array != NULL)
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+			{
+				return (i);
+			}
+		}
+	return (-1);
 }
