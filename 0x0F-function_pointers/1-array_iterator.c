f7cec5a51967844e58ef6d4532e44ef251e3e763
nosakhare <nosakhareaghedo42@gmail.com>
2022-05-13 21:09:33 +0000

    printing iteratively

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,10 +11,8 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
-	
-	if (action == NULL)
-		return;
-	else
+
+	if (action != NULL)
 	{
 		for (i = 0; i < size; i++)
 		{
