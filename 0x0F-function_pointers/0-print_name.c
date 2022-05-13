85cf5afc4530f720125a490319bdde1b943883ad
nosakhare <nosakhareaghedo42@gmail.com>
2022-05-13 20:00:48 +0000

    printing name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_name - function to print name
+ * @name: var containing name
+ * @f: address to function
+ */
+void print_name(char *name, void (*f)(char *))
+{    
+       	f(name); 
+}	
