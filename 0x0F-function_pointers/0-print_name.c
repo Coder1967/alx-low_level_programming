863e2ae5a0fc0c2ca5f0eceebf487ae0f98b4a85
nosakhare <nosakhareaghedo42@gmail.com>
2022-05-13 20:20:07 +0000

    printing name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 /**
  * print_name - function to print name
  * @name: var containing name
@@ -7,7 +8,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if(f != NULL)
+	if(*f != NULL)
 	{
 		f(name);
 	}
