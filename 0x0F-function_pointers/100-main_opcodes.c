1d7d0f21639f20726a4a05d7a1f36486cf23b0c3
Coder1967 <nosakhareaghedo42@gmail.com>
2022-05-13 17:42:10 -0700

    opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -5,18 +5,11 @@
 
 
 /**
- *
- *  * main - prints its own opcodes
- *
- *   * @argc: number of arguments
- *
- *    * @argv: array of arguments
- *
- *     *
- *
- *      * Return: Always 0 (Success)
- *
- *       */
+ * main - prints its own opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0 (Success)
+ */
 
 int main(int argc, char *argv[])
 
