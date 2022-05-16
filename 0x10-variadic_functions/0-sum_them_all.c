2d4e8d2240e1665fe71bcf0bc8066583e8aabd6e
nosakhare <nosakhareaghedo42@gmail.com>
2022-05-16 14:57:25 +0000

    problem with header files fixed

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,16 +9,16 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	int i, x, sum = 0;
+	va_list args;
 
 	if (n == 0)
 	{
 		return (0);
 	}
-	va_list args;
 
 	va_start(args, n);
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < (int)n; i++)
 	{
 		x = va_arg(args, int);
 		sum += x;
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,7 +17,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 		va_start(args, n);
 
-		for (i = 0; i < n; i++)
+		for (i = 0; i < (int)n; i++)
 		{
 			x = va_arg(args, int);
 			printf("%d", x);
