c97c35ed33c6f6dddd4deda4c2c7bb074b93c39c
nosakhare <nosakhareaghedo42@gmail.com>
2022-05-24 23:19:30 +0000

    constructor added

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
  * printer - function to print before main is called
+ * Return: 0
  */
 void __attribute__((constructor)) printer()
 {
-	printf("You're beat! and yet, you must allow,
-			\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
