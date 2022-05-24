1e48d8ef38e5921e1dd0d3b9cbac0c3521eaa981
nosakhare <nosakhareaghedo42@gmail.com>
2022-05-24 23:11:47 +0000

    constructor added

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * printer - function to print before main is called
+ */
+void __attribute__((constructor)) printer();
+{
+	printf("You're beat! and yet, you must allow,
+			\nI bore my house upon my back!\n");
+}
