891904726be15e609353b20d5bc3cfeb7da6a1d4
nosakhare <nosakhareaghedo42@gmail.com>
2022-05-24 14:06:09 +0000

    added prototypes

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,3 +1,5 @@
+#ifndef LIST_H
+#define LIST_H
 /** 
  * struct list_s - singly linked list 
  *  @str: string - (malloc'ed string)  
@@ -12,3 +14,6 @@ typedef struct list_s
    	struct list_s *next; 
 } 
 list_t;
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+#endif
