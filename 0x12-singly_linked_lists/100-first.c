243a8067a1c19a50351529a733c30c04729d47c7
nosakhare <nosakhareaghedo42@gmail.com>
2022-05-24 23:12:34 +0000

    constructor added

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,7 +2,7 @@
 /**
  * printer - function to print before main is called
  */
-void __attribute__((constructor)) printer();
+void __attribute__((constructor)) printer()
 {
 	printf("You're beat! and yet, you must allow,
 			\nI bore my house upon my back!\n");
