6f96ef05372a7c56947d51594d8f58a9d6f0b0ca
nosakhare <nosakhareaghedo42@gmail.com>
2022-05-24 20:59:10 +0000

    freed list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_list - function to free allocated memory for linked list
+ * @head: pointer to the first node
+ * Return: nothing(void)
+ */
+void free_list(list_t *head)
+{
+	list_t tmp;
+
+	while (tmp != NULL)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
