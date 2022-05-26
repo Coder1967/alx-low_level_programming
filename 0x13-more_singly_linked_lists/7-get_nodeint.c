7941b0f4591773e66e041b99133857cd2b15138c
nosakhare <nosakhareaghedo42@gmail.com>
2022-05-26 22:02:00 +0000

    added a function to return the nth node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,11 +9,13 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
+	unsigned int i;
+
 	if (head == NULL)
 	{
 		return (NULL);
 	}
-	for (unsigned int i = 0; i < index - 1; i++)
+	for (i = 0; i < index - 1; i++)
 	{
 		head = head->next;
 	}
