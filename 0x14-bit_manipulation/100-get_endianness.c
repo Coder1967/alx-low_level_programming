aa552caea3e87210bc5322e1a17953eb36c8a82c
nosakhare <nosakhareaghedo42@gmail.com>
2022-05-30 22:57:33 +0000

    endian checker

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * get_endianess - function to get the endianess of a computer
+ * Return: 1 if small endian and 0 otherwise
+ */
+int get_endianness(void)
+{
+	unsigned int n = 1;
+	if (*(char *) &n == 1)
+	{
+		return (1);
+	}
+	return (0);
+}
