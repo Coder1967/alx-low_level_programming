81902837ac54b31ea02c7a43eed5377db94cc91f
nosakhare <nosakhareaghedo42@gmail.com>
2022-05-30 22:19:42 +0000

    function to set indxed bit added

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * set_bit - sets thje bit of the indexed number to 1
+ * @n: number whose bit is to be set to 1
+ * @index: the indexed bit to be set
+ * Return: 1(on sucess)
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > (sizeof(unsigned long int) * 7))
+		return (-1);
+	*n = n ^ (1 << index);
+	return (1);
+}
