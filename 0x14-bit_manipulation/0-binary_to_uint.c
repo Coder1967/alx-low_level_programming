eb2c91ed9ef0de7c28b7199049f565c5ecf247e5
nosakhare <nosakhareaghedo42@gmail.com>
2022-05-30 20:24:28 +0000

    function to convert binary to decimal

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,35 +8,38 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int n = atoi(b), j;
+	unsigned int n, j;
 	unsigned int tmp = 0, total = 0, count = 0;
-	unsigned int i = 0, len = strlen(b);
+	unsigned int i = 0, len;
 
-	for (j = 0; j < len; j++)
+	if (b != NULL)
 	{
-		if (b[j] == '0' || b[j] == '1')
-			count++;
-	}
+		n = atoi(b);
+		len = strlen(b);
 
-	if (b == NULL)
-	{
-		return (0);
+		for (j = 0; j < len; j++)
+		{
+			if (b[j] == '0' || b[j] == '1')
+			{
+				count++;
+			}
+		}
+		while (n != 0)
+		{
+			tmp = n % 10;
+			tmp <<= i;
+			total += tmp;
+			n = n / 10;
+			i++;
+		}
+		if (count == len)
+		{
+			return (total);
+		}
+		else
+		{
+			return (0);
+		}
 	}
-
-	while (n != 0)
-	{
-		tmp = n % 10;
-		tmp <<= i;
-		total += tmp;
-		n = n / 10;
-		i++;
-	}
-	if (count == len)
-	{
-		return (total);
-	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
