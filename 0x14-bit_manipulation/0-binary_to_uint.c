b244b697a8e6581657b9cff72ac52aed0c0edd9b
nosakhare <nosakhareaghedo42@gmail.com>
2022-05-30 20:05:13 +0000

    function to convert binary to decimal

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,30 +1,33 @@
-#include <stdlib.h> 
+#include <string.h>
 #include "main.h"
+#include <stdlib.h>
 /**
- * binary_to_unit - converts binary to decimal
+ * binary_to_uint - converts binary to decimal
  * @b: base 2 to be converted supplied as a string
  * Return: decimal conversion(on success);
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int n = atoi(b);
-	unsigned int tmp = 0, total = 0;
-	int i = 0;
+	unsigned int n = atoi(b), j;
+	unsigned int tmp = 0, total = 0, count = 0;
+	unsigned int i = 0, len = strlen(b);
 
-	if (b != NULL)
+	for (j = 0; j < len; j++)
 	{
-		if (b[i] == 48 || b[i] = 49 || b[i] = '\0')
-		{
-			while(n != 0)
-			{
-				tmp = n % 10;
-				tmp <<= i;
-				total += tmp;
-				n = n / 10;
-				i++;
-			}
-			return (total);
-		}
+		if (b[j] == '0' || b[j] == '1')
+			count++;
 	}
-return (0);	
+	while (n != 0)
+	{
+		tmp = n % 10;
+		tmp <<= i;
+		total += tmp;
+		n = n / 10;
+		i++;
+	}
+	if (count == len && b != NULL)
+	{
+		return (total);
+	}
+	return (0);
 }
