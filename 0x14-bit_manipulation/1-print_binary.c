1a9bb1cb2e7c69d99e694aa7266d2a02862d9e2a
nosakhare <nosakhareaghedo42@gmail.com>
2022-05-30 21:10:35 +0000

    decimal to binary converter

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,6 +1,4 @@
 #include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "main.h"
 /**
  * print_binary - function to print binary number
@@ -9,7 +7,7 @@
  */
 void print_binary(unsigned long int n)
 {
-	if (n > 1) 
-		print_binary(n >> 1); 
-	_putchar((n & 1) + '0'); 
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
