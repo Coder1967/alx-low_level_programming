512eecde9d0e620c5b771861bde894a1bc001fdc
nosakhare <nosakhareaghedo42@gmail.com>
2022-06-01 19:39:33 +0000

    reading from a file then writing to standard output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stddef.h>
+/**
+ * read_textfile - function to read from a file and print it to
+ * standard output
+ * @filename: name of file to be read
+ * @letters: amount of letters in file
+ * Return: amount of numbers read
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	if (filename != NULL)
+	{
+		ssize_t sz;
+		int fd;
+		char *buf = (char *) malloc(sizeof(letters) + 1);
+		
+		fd = open(filename, O_RDONLY);
+		if (fd == 0)
+		{
+			free(buf);
+			return (0);
+		}
+		sz = read(fd, buf, letters);
+		buf[letters] = '\0';
+		close(fd);
+		if (buf - 1 != letters)
+		{
+			free(buf);
+			return (0);
+		}
+		write(1, buf, sizeof(buff));
+	}
+	free(buf);
+	return (0);
+}
+
