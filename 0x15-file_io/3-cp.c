2eb53a15e170bac1a2234c6ba8d5adefd23da56c
nosakhare <nosakhareaghedo42@gmail.com>
2022-06-01 22:49:00 +0000

    a function that copies and pasta

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,8 +1,8 @@
 #include <unistd.h>
-#include <fcntl.h> 
+#include <fcntl.h>
 #include <sys/stat.h>
-#include <sys/types.h> 
-#include <stdlib.h> 
+#include <sys/types.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 /**
@@ -13,7 +13,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int fd, sd, cd, rd;
+	int fd1, fd2, sd, cd, rd;
 	char buf[1024];
 
 	if (argc != 3)
@@ -21,22 +21,22 @@ int main(int argc, char *argv[])
 		write(2, "Usage: cp file_from file_to\n", 29);
 		exit(97);
 	}
-	fd = open(argv[1], O_RDONLY | O_TRUNC, 0600);
-	if (fd == -1)
+	fd1 = open(argv[1], O_RDONLY | O_TRUNC, 0600);
+	if (fd1 == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	rd = read(fd, buf, 1024);
-	close(fd);
-	fd = open(argv[2], O_CREAT | O_WRONLY, 0644);
-	sd = write(fd, buf, rd);
-	if (fd == -1 || sd == -1)
+	rd = read(fd1, buf, 1024);
+	fd2 = open(argv[2], O_CREAT | O_WRONLY, 0644);
+	sd = write(fd2, buf, rd);
+	if (fd2 == -1 || sd == -1)
 	{
 		dprintf(2, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	cd = close(fd);
+	close(fd1);
+	cd = close(fd2);
 
 	if (cd == -1)
 	{
