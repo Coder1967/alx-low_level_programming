44b8d7746399173de8cdaa78be4e0f564bc85d0f
nosakhare <nosakhareaghedo42@gmail.com>
2022-06-01 21:40:32 +0000

    function to create file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -24,7 +24,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		close(fd);
 		return (1);
 	}
-	write(sd, text_content, strlen(text_content) * sizeof(char));
+	write(fd, text_content, strlen(text_content) * sizeof(char));
 	close(fd);
 	return (1);
 }
