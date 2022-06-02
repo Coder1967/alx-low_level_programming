94bb28c895701d4ba0041ad2733788f96cf51f2f
nosakhare <nosakhareaghedo42@gmail.com>
2022-06-02 21:33:19 +0000

    fixed copy function

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,29 +18,32 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		write(2, "Usage: cp file_from file_to\n", 29);
+		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	fd1 = open(argv[1], O_RDWR, 0666);
-	rd = read(fd1, buf, 1024);
-	if (fd1 == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
 	fd2 = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0644);
-	sd = write(fd2, buf, rd);
-	if (fd2 == -1 || sd == -1)
+
+	while ((rd = read(fd1, buf, 1024)) != 0)
 	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+		if (rd == -1)
+		{
+			dprintf(2, "Error:Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		sd = write(fd2, buf, rd);
+		if (fd2 == -1 || sd == -1)
+		{
+			dprintf(2, "Error:Can't write to %s\n", argv[2]);
+			exit(99);
+		}
 	}
 	close(fd1);
 	cd = close(fd2);
 
 	if (cd == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd2);
+		dprintf(2, "Error:Can't close fd %d\n", fd2);
 		exit(100);
 	}
 	return (0);
