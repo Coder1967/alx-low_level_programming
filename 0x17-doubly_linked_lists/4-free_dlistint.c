a69f997d28e367f8e6ca64763027af764a875cc4
nosakhare <nosakhareaghedo42@gmail.com>
2022-07-07 13:50:24 +0000

    added more functions for the doubly linked list

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -7,12 +7,12 @@
  */
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *tmp = head;
+	dlistint_t *tmp = head->next;
 
-	while (head != NULL)
+	while (tmp != NULL)
 	{
-		tmp = head->next;
 		free(head);
 		head->next = tmp;
+		tmp = tmp->next;
 	}
 }
