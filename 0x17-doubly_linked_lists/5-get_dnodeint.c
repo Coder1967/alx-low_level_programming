968fbd4d3ce4ac915652ba68b1f39b806ced2955
nosakhare <nosakhareaghedo42@gmail.com>
2022-07-07 14:12:02 +0000

    added more functions for the doubly linked list

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+/**
+ * get_dnode_at_index - function gets the address of the indexed
+ * node starting count from 0
+ * @head: pointer to head node
+ * @index: the index of the node to be returned
+ * Return: address of specified node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *tmp = head;
+	unsigned int size = 0, i;
+
+	if (head == NULL)
+		return (NULL);
+
+	while (tmp != NULL)
+	{
+		tmp = tmp->next;
+		size++;
+	}
+	tmp = head;
+	if (index > size - 1)
+		return (NULL);
+
+	for (i = 0; i < index; i++)
+	{
+		tmp = tmp->next;
+	}
+	return (tmp);
+}
