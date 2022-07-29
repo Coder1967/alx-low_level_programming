94736fa272c577ae7e6a7214666f8ec319d3f398
Nosakhare aghedo <nosakhareaghed042@gmail.com>
2022-07-29 14:11:03 +0000

    created an hash map

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,12 +9,14 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned int index = key_index((const unsigned char *)key, ht->size);
+	char *k = strdup(key);
 	hash_node_t *tmp = ht->array[index];
 
 	while (tmp != NULL)
 	{
-		if (strcmp(tmp->key, key) == 0)
+		if (strcmp(tmp->key, k) == 0)
 		{
+			free(k);
 			return (tmp->value);
 		}
 		tmp = tmp->next;
