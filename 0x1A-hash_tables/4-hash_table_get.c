ce9122c62a836df752faa609e92daf946a2d6fb6
Nosakhare aghedo <nosakhareaghed042@gmail.com>
2022-07-29 14:00:01 +0000

    created an hash map

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+#include <string.h>
+/**
+ * hash_table_get - uses the key to return a value
+ * @ht - pointer to the hash table
+ * @key: the key whose value is to be returned
+ * Return: value(on success) and NULL(on failure)
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned int index = key_index((const unsigned char *)key, ht->size);
+	hash_node_t *tmp = ht->array[index];
+
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			return (tmp->value);
+		}
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
