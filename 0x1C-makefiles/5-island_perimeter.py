fdfa906a7137c692f67af7029991345620a7e1ba
nosakhare aghedo <nosakhareaghedo42@gmail.com>
2022-09-14 21:43:37 +0100

    added python file

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,22 +2,24 @@
 """
 module to calculate the perimeter of the island described in grid
 """
-def island_perimeter(grid):
-        """
-        returns the perimeter of the island described in 'grid'
-        """
-        width = len(grid[0])
-        height = len(grid)
-        size = 0
 
-        for i in range(height):
-            for j in range(width):
-                if grid[i][j] == 1:
+
+def island_perimeter(grid):
+    """
+    returns the perimeter of the island described in 'grid'
+    """
+    width = len(grid[0])
+    height = len(grid)
+    size = 0
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+            if i != 0:
+                if grid[i - 1][j] == 1 and grid[i][j + 1] == 1:
                     size += 1
-                if i != 0:
-                    if grid[i - 1][j] == 1 and grid[i][j + 1] == 1:
-                        size += 1
-                if i != height - 1:
-                    if grid[i + 1][j] == 1 and grid[i][j + 1] == 1:
-                        size += 1
-        return size * 2
+            if i != height - 1:
+                if grid[i + 1][j] == 1 and grid[i][j + 1] == 1:
+                    size += 1
+    return size * 2
